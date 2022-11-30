c1c91fd3a0c858704589a87be97c92c88f3fa477
Hynn <choihwoong@me.com>
2022-11-30 15:38:46 +0900

    feat : Javascript updated

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 const menuicon = document.querySelector('#header > #menuicon')
 const navmenu = document.querySelector('#header > #navmenu')
 const menuItem = document.querySelectorAll ('#header > #navmenu > li')
-
-
+const ContactDB = document.querySelector('#Contact_input')
+const Name = document.querySelector('#con_name')
+const Email = document.querySelector('#con_mail')
+const Subject = document.querySelector('#con_subject')
+const Body = document.querySelector('#con_body')
+const btn = document.querySelector('.btn')
 
 function EventHandler(e){
     navmenu.classList.add('on')
@@ -11,6 +15,17 @@ function EventHandler(e){
 function EventHandler2(e){
     navmenu.classList.remove('on')
 }
+
+function EventHandler3(e){
+    console.log('hello')
+    if (Name.value == null, Email.value == null,Subject.value == null,Body.value == null) {
+        alert('Please Insert your Information')
+    }else {
+        alert('Thanks for Contact Hynn, We will soon update your Contact Information')       
+    }
+    }   
+
+btn.addEventListener('click', EventHandler3)
 
 menuicon.addEventListener('click', EventHandler)
 navmenu.addEventListener('click', EventHandler2)
